8c52d7b62d63a266977ab25408a67d60a1d39978
tzzt816 <2118176174@qq.com>
2024-08-13 23:35:51 +0800

    rename the title of window

--- src/main.cc
+++ src/main.cc
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Hello World -- Nya!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window *win = SDL_CreateWindow("demoAirplane", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 
     if (win == nullptr) {
         std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
